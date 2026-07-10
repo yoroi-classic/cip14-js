f02cb77152e68f57ceb903c84c8850a3662b0fd2
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-07-10 09:01:52 -0400

    chore: modernize TypeScript package toolchain

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  transform: {'^.+\\.ts?$': 'ts-jest'},
-  testEnvironment: 'node',
-  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  transform: {
+    "^.+\\.ts?$": ["ts-jest", { tsconfig: "tsconfig.typecheck.json" }],
+  },
+  testEnvironment: "node",
+  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
